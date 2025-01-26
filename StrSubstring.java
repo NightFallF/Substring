33d8f4fab57add4565b540924653127a5c4f2408
NightFallF <nightfallzadh@gmail.com>
2025-01-26 16:06:50 +0530

    Add files via upload

---
+++ StrSubstring.java
@@ -0,0 +1,7 @@
+public class StrSubstring {
+    public static void main(String[] args) {
+        String word = "Artha";
+        String Subword = word.substring(0, 3);
+        System.out.printf("The substring of \"%s\" is: %s", word, Subword);
+    }
+}
